e03ddd872ec6a64e270fcdfc2a58ae583e9bf4cb
Tushar <tm2582001@gmail.com>
2023-02-05 14:17:17 +0530

    added user saga logic

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 import FormInput from "../form-input/form-input.component";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 import {
-  signInAuthUserWithEmailAndPassword,
-  signInWithGooglePopup,
-} from "../../utils/firebase/firebase.utils";
+  googleSignInStart,
+  emailSignInStart,
+} from "../../store/user/user.action";
 
 import "./sign-in-form.styles.scss";
 
@@ -16,6 +17,7 @@ const defaultFormFields = {
 };
 
 const SignInForm = () => {
+  const dispatch = useDispatch();
   const [formFields, setFormFields] = useState(defaultFormFields);
 
   const { email, password } = formFields;
@@ -23,7 +25,7 @@ const SignInForm = () => {
   // console.log(formFields);
 
   const signInWithGoogle = async () => {
-    await signInWithGooglePopup();
+    dispatch(googleSignInStart());
   };
 
   const resetFormFields = () => {
@@ -39,7 +41,7 @@ const SignInForm = () => {
     event.preventDefault();
 
     try {
-      await signInAuthUserWithEmailAndPassword(email, password);
+      dispatch(emailSignInStart(email, password));
 
       resetFormFields();
     } catch (error) {
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 
-import FormInput from '../form-input/form-input.component';
-
-import { createAuthUserWithEmailAndPassword,createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import FormInput from "../form-input/form-input.component";
 
 import Button from "../button/button.component";
+import { signUpStart } from "../../store/user/user.action";
 
-import './sign-up-form.styles.scss';
+import "./sign-up-form.styles.scss";
 
 const defaultFormFields = {
   displayName: "",
@@ -17,15 +17,14 @@ const defaultFormFields = {
 
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
-
   const { displayName, email, password, confirmPassword } = formFields;
-
+  const dispatch = useDispatch();
 
   // console.log(formFields);
 
-  const resetFormFields = ()=>{
+  const resetFormFields = () => {
     setFormFields(defaultFormFields);
-  }
+  };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -40,19 +39,12 @@ const SignUpForm = () => {
       return;
     }
     try {
-      const {user} = await createAuthUserWithEmailAndPassword(
-        email,
-        password
-      );
-      
-      await createUserDocumentFromAuth(user, {displayName});
-
+      dispatch(signUpStart(email, password, displayName));
       resetFormFields();
-
     } catch (error) {
-      if(error.code === 'auth/email-already-in-use'){
-        alert('Cannot create user, email already in use');
-      }else console.log("user created encountered an error", error);
+      if (error.code === "auth/email-already-in-use") {
+        alert("Cannot create user, email already in use");
+      } else console.log("user created encountered an error", error);
     }
   };
 
