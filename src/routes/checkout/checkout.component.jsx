917ac7293f5a1690dfde872cdba11f24c01ca524
Tushar <tm2582001@gmail.com>
2023-02-06 12:27:19 +0530

    added serverless function and client side code for stripe payments

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 
 import "./checkout.styles.scss";
 
@@ -31,7 +32,8 @@ const Checkout = () => {
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <span className="total">Total: ${cartTotal}</span>
+      <span className="total">Total: &#8377;{cartTotal}</span>
+      <PaymentForm />
     </div>
   );
 };
