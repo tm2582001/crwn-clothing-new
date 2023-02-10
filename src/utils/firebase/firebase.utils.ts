5954a440e565e7a0ee0727c569eab822d8a6122b
Tushar <tm2582001@gmail.com>
2023-02-10 10:42:59 +0530

    migrated firebase util and user action, reducer, type, selector to ts

---
+++ src/utils/firebase/firebase.utils.ts
@@ -0,0 +1,156 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  User,
+  NextOrObserver
+} from "firebase/auth";
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+  QueryDocumentSnapshot
+} from "firebase/firestore";
+
+import { Category } from "../../store/categories/category.types";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDEoqOh7kohjlvgFpCz1udHgRnc1wNa4A",
+  authDomain: "crwn-cloathing-db-8f816.firebaseapp.com",
+  projectId: "crwn-cloathing-db-8f816",
+  storageBucket: "crwn-cloathing-db-8f816.appspot.com",
+  messagingSenderId: "970466026331",
+  appId: "1:970466026331:web:4b15ec2460fcf7b0ca307e",
+};
+
+// Initialize Firebase
+// const firebaseApp = 
+initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export type ObjectToAdd = {
+  title: string;
+};
+
+export const addCollectionAndDocuments = async <T extends ObjectToAdd>(
+  collectionKey: string,
+  objectsToAdd: T[]
+): Promise<void> => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docref = doc(collectionRef, object["title"].toLowerCase());
+    batch.set(docref, object);
+  });
+
+  await batch.commit();
+  console.log("done");
+};
+
+export const getCategoryAndDocuments = async (): Promise<Category[]> => {
+  const collectionRef = collection(db, "categories");
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data() as Category);
+};
+
+export type AdditionalInformation = {
+  displayName?: string;
+}
+
+export type UserData = {
+  createdAt: Date;
+  displayName: string;
+  email: string;
+}
+
+export const createUserDocumentFromAuth = async (
+  userAuth: User,
+  additionalInformation = {} as AdditionalInformation
+): Promise<void | QueryDocumentSnapshot<UserData>> => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  // console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  // console.log(userSnapshot);
+  // console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    //bang operator
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log("error creating the user", error);
+    }
+  }
+
+  return userSnapshot as QueryDocumentSnapshot<UserData>;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email: string, password: string) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email: string, password: string) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback: NextOrObserver<User>) =>
+  onAuthStateChanged(auth, callback);
+
+export const getCurrentUser = (): Promise<User | null> => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (userAuth) => {
+        unsubscribe();
+        resolve(userAuth);
+      },
+      reject
+    );
+  });
+};
